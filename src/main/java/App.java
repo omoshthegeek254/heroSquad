2d218f40fe624c579eb178a15771fa9e674ce7f9
omoshthegeek <omoshthegeek@gmail.com>
2020-01-01 23:49:23 +0300

    added the main layout file

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
+public class App {
+    public static void main (String [] args){
+        staticFileLocation( "/public");
+
+        get("/", (request, response) -> {
+
+            return new ModelAndView(null,"master.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+
+}

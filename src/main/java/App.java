0e6b421f46183025c5ee514dde373c2cc01c0740
omoshthegeek <omoshthegeek@gmail.com>
2020-01-02 21:10:35 +0300

    added the final web routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,7 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App {
     public static void main (String [] args){
@@ -11,6 +10,16 @@ public class App {
         get("/", (request, response) -> {
 
             return new ModelAndView(null,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post ("/post-hero", (request, response) -> {
+
+            return new ModelAndView(null,"post-hero.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post ("/hero-team", (request, response) -> {
+
+            return new ModelAndView(null,"hero-team.hbs");
         }, new HandlebarsTemplateEngine());
     }
 

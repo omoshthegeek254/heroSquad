52b5c25b666b87215c2dee74934cf5276250362d
omoshthegeek <omoshthegeek@gmail.com>
2020-01-02 21:12:30 +0300

    added the the logic for the heroes object

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,9 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
@@ -13,6 +16,13 @@ public class App {
         }, new HandlebarsTemplateEngine());
 
         post ("/post-hero", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            String name = request.queryParams("name");
+            int age = Integer.parseInt(request.queryParams("age"));
+            String superPowers = request.queryParams("superPowers");
+            String weakness = request.queryParams("weakness");
+
 
             return new ModelAndView(null,"post-hero.hbs");
         }, new HandlebarsTemplateEngine());

883008a2332d75e8c94cf37a5e00cc6fd9f53fee
omoshthegeek <omoshthegeek@gmail.com>
2020-01-08 09:28:10 +0300

    added test layout

---
+++ src/test/java/models/HeroesTest.java
@@ -0,0 +1,32 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeroesTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getAge() {
+    }
+
+    @Test
+    public void getSuperPowers() {
+    }
+
+    @Test
+    public void getWeakness() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getAllHeroes() {
+    }
+}
---
+++ src/test/java/models/SquadsTest.java
@@ -0,0 +1,28 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SquadsTest {
+
+    @Test
+    public void getSquadName() {
+    }
+
+    @Test
+    public void getSquadMembers() {
+    }
+
+    @Test
+    public void getSquadCause() {
+    }
+
+    @Test
+    public void getAllSquads() {
+    }
+
+    @Test
+    public void getId() {
+    }
+}
